021ac23b9d72cc8998b8ac80aa077665f0a30dfd
Tomohiro Sasaki <takatobi0326@icloud.com>
2020-12-05 00:12:19 +0900

    Create index.php

---
+++ 2-11/index.php
@@ -0,0 +1,17 @@
+<form action="result.php" method="post">
+
+名前：<input type="text" name="my_name" />
+<br>
+メールアドレス:<input type="text" name="e-mail"/>
+<br>
+パスワード：<input type="password" name="pass"/>
+<br>
+<input type="submit" value="送信"/>
+
+</form>
+
+
+仕様書：ソフトウェア等を開発する際に備えるべき機能や性能を図表や文章などで記述した文書（何を作るか）
+設計書：システム開発をするに当たって、重要になる計画を立てる工程のこと（システムをどのように作るか）
+Git:ファイルやディレクトリにいつ誰が変更したかを記録できる（履歴を管理できる）もの
+マージツール:GITのツールの一つ。ブランチを統合すること。統合することで、データを読み込み、それを使うことができる
