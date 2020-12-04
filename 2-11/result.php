b581080b5bb989659a12ebe9bbe3cdcc403a0dee
Tomohiro Sasaki <takatobi0326@icloud.com>
2020-12-05 00:12:23 +0900

    Create result.php

---
+++ 2-11/result.php
@@ -0,0 +1,16 @@
+<?php
+$my_name=$_POST['my_name'];
+$email=$_POST['e-mail'];
+$pass=$_POST['pass'];
+
+echo '私の名前は、'.$my_name;
+echo '<br>';
+echo 'メールアドレスは、'.$email;
+echo '<br>';
+echo '私のパスワードは、'.$pass;
+
+
+
+
+
+?>
